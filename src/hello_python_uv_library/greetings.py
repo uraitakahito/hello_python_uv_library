b3e1cd6bc60b752ea24d00ce5c47edbcf3d234cf
Takahito Urai <uraitakahito@gmail.com>
2025-03-18 13:56:46 +0900

    Bump version to 0.1.4, add hello_add function, and update tests

--- src/hello_python_uv_library/greetings.py
+++ src/hello_python_uv_library/greetings.py
@@ -1,2 +1,9 @@
+from hello_python_add import calc
+
+
 def hello():
     return "Hello, Python UV Library!"
+
+
+def hello_add(a: int, b: int) -> int:
+    return calc.add(a, b)
--- tests/test_hello_python_uv_library/test_hello.py
+++ tests/test_hello_python_uv_library/test_hello.py
@@ -3,3 +3,7 @@ from hello_python_uv_library import greetings
 
 def test_hello():
     assert greetings.hello() == "Hello, Python UV Library!"
+
+
+def test_hello_add():
+    assert greetings.hello_add(1, 2) == 3
